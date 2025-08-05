ad16ab3a8909f5f15cbba4e64ae187d23d9c1e03
001vamp <dirtymycup@gmail.com>
2025-08-05 06:00:21 -0400

    Force add all files

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+let js = 'slat';
+
+console.log(40 + 8 + 23 - 10);
+console.log("seanna")
+console.log(23);
+
+let value = "Kyana";
+
+
+console.log(value);
+console.log(value);
+console.log(value);
+
+
+let country = "USA, ";
+
+let continent = 'Wtv, ';
+
+let population = '9 gazillion';
+
+console.log(continent + country + population);
+
+/* at this moment, i realized why we write js using objects :3 interesting */
+let slimeStatus = "Slime";
+
+if (slimeStatus === `Slime`) {
+
+    console.log("Im Slatt Im Slime Hoe 🟢🐍");
+
+} else {
+
+    console.log(`Not Slime :(`);
+
+}
