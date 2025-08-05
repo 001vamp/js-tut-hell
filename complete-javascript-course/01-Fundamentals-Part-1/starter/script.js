cfc5486792725d31cc2010656dfd53991f5dd9fb
001vamp <dirtymycup@gmail.com>
2025-08-05 15:12:35 -0400

    Finished Fundamentals - Part 1 onto Part 2

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -34,3 +34,62 @@ if (slimeStatus === `Slime`) {
 }
 
 // Finished 17 - Continue Fundamentals. 
+
+const inputYear = '1991';
+
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 11);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+console.log(typeof 'Jonas');
+
+
+const day = `wednesday`;
+
+switch (day) {
+    case `monday`: // day === `monday`
+        console.log(`Go to Work`);
+        console.log(`Do something Like Code`);
+        break;
+
+    case `tuesday`:
+        console.log(`same shi`)
+        break;
+
+    case `wednesday`:
+        console.log(`lowkey fw humpdayz`)
+        break;
+    default: console.log('idek what day it is')
+}
+
+// good for comapring a value with multiple values.
+
+if (day === `tuesday`) {
+    console.log(`The day is Tuesday <3`);
+}
+else if (day === `wednesday`) {
+    console.log(`Hump DAYYYYY`);
+} else {
+    console.log(`We dont know what day it is fr`)
+}
+
+
+
+// Anything that produces a value = a expression
+
+// if else are statements that do actions aka statements = do actions
+
+// so Loops, Switch etc
+
+// you cannot put Statements in Temp Lit ${}
+
+const age = 22
+
+age >= 22 ? console.log('slatt') :
+    console.log('not slatty');
+
+age > 22 ? console.log('less than 22') :
+    console.log(`over 22`);
