bb143f4fe94bc14c852092b4a3901cb21f6bcd89
001vamp <dirtymycup@gmail.com>
2025-08-16 13:27:44 -0400

    No More Modals, Will Be Focusing on Core Js concepts.

--- complete-javascript-course/06-Modal/starter/script.js
+++ complete-javascript-course/06-Modal/starter/script.js
@@ -12,3 +12,10 @@ console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++) {
     console.log(btnsOpenModal[i].textContent)
 }
+
+
+
+
+// fuck this i dont care for modals right now i truly care about learning js more and becoming good at leet code
+// even tho it seems like leet code sucks but i just need something that helps me set a good foundation on the lang. 
+// and doing MODALS rn is boring and not hitting the seratonin in my brain. 
