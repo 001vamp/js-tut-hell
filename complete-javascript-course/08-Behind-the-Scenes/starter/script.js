023f465c72b429338196041658da53a464f0f587
001vamp <dirtymycup@gmail.com>
2025-08-17 14:40:37 -0400

    Lowish Levelish review. + idek my brain hurts prob should stop coding on weekends or sum

--- complete-javascript-course/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course/08-Behind-the-Scenes/starter/script.js
@@ -1,3 +1,24 @@
 'use strict';
 // starting this
 // rather do a low level review anyways
+// compilation vs interpration
+// execution
+
+
+
+/*Js runs in execution context, it runs code outside of defined functions first 
+in the EC - necessary info for code to be executed is stored.
+
+so once the top level code that isnt in function executes
+it runs functions and waits for callbacks.
+for each function call a new execution context is created
+*/
+
+/* inside of execution context? 
+
+Variable Enviorment
+
+Scope Chain
+
+*/
+
